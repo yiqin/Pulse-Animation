33512bbc69bc85be510fbdaa6f02def572d237c8
yiqin <yiqin.mems@gmail.com>
2014-08-09 18:11:55 -0400

    Update codes.

--- YQPulseAnimation-Example/YQViewController.swift
+++ YQPulseAnimation-Example/YQViewController.swift
@@ -10,15 +10,22 @@ import UIKit
 
 class YQViewController: UIViewController {
     
-    var pulsingLayer = YQPulsingLayer()
+    // var pulsingLayer = YQPulsingLayer()
     @IBOutlet var objectView : UIImageView = nil
                             
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        pulsingLayer.position = self.objectView.center
-        self.view.layer.insertSublayer(pulsingLayer, below: objectView.layer)
+        
+        // declare a pulsing layer
+        var pulsingLayer = YQPulsingLayer()
+        
+        // place the pulsing layer in the center the object view
+        pulsingLayer.position = objectView.center
+        
+        // add the pulsing layer to the view
+        view.layer.insertSublayer(pulsingLayer, below: objectView.layer)
     }
 
     override func didReceiveMemoryWarning() {
